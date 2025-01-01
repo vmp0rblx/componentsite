b426cb0a617c0ab815d696c35d1e3a1c7eaef671
Peyton / .vmp0 <68137205+vmp0rblx@users.noreply.github.com>
2025-01-01 16:15:42 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import componentLogo from './assets/logotrans.png'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://discord.gg/24czpdUy5q" target="_blank">
+        <a href="https://discord.gg/24czpdUy5q" target="_blank" rel="noopener noreferrer">
           <img src={componentLogo} className="logo" alt="Component" />
         </a>
       </div>
